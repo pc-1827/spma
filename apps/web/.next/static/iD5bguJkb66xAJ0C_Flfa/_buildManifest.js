fcb783b7fd0722fe532626b503315bf627bc62e6
pc-1827 <problemcreator1827@gmail.com>
2025-01-28 08:53:45 +0530

    temp commit 2

---
+++ apps/web/.next/static/iD5bguJkb66xAJ0C_Flfa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e71b18a75264c1a1.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
